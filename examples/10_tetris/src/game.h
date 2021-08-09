53f66544275afa1a5467c84ef0695b4af1d24eed
Juhang Park <juhang3@daum.net>
2021-08-09 19:27:32 +0900

    tetris, tiled

--- examples/10_tetris/src/game.h
+++ examples/10_tetris/src/game.h
@@ -17,8 +17,7 @@ void ProcessLogic();
 char CheckGameEnd();
 void DrawWorld();
 void DrawBoard();
-void DrawWall();
-void UpdateBall() ;
+void DrawNextPiece() ;
 
 extern void RenderRect(int x1, int y1, int x2, int y2, int r, int g, int b, int fill); //사각형을 그리는 함수
 extern void RenderCircle(int x, int y, int radius, int r, int g, int b); // 원을 그리는 함수
